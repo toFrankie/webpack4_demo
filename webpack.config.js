54a3062ce421b3c7e158c307a70516047f5513b0
Frankie <1426203851@qq.com>
2020-08-20 09:19:30 +0800

    add plugin、entry、devServer、mode、devtool

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,28 @@
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 const config = {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  entry: {
+    index: path.resolve(__dirname, './src/index.js')
+  },
+  devServer: {
+    contentBase: './dist',
+    open: true
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Hi', // 模板要使用 <title><%= htmlWebpackPlugin.options.title %></title> 配置才生效
+      title: '开发环境', // 模板要使用 <title><%= htmlWebpackPlugin.options.title %></title> 配置才生效
       template: './src/index.html', // 模板路径
       filename: 'index.html', // 输出 HTML 文件名称
       inject: 'body', // 插入的 script 标签位于 body 底部，true 同理
       hash: true, // 加上 hash 值
       favicon: './src/favicon.ico'
-    })
+    }),
+    // 新版无需再指定删除目录，默认删除 output 目录
+    new CleanWebpackPlugin()
   ]
 }
 
