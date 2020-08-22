be5c10b1f4e7388252bc8da86bc687b1918c9de2
Frankie <1426203851@qq.com>
2020-08-23 03:25:48 +0800

    React 示例

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import React from 'react'
+import { render } from 'react-dom'
 import './main.js'
 import './style.css'
 
-console.log('Hello Webpack!')
+// 最简单的 React 示例
+const rootElem = document.getElementById('app')
+render(<div>Hello React!</div>, rootElem)
 
 if(module.hot) {
     module.hot.accept('./main.js', () => {
