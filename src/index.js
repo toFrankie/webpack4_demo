2bffbb2736475478d067a4cfb4d6bef303623572
Frankie <1426203851@qq.com>
2020-08-22 22:32:03 +0800

    HMR setting

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import './main.js'
+import './style.css'
+
 console.log('Hello Webpack!')
+
+if(module.hot) {
+    module.hot.accept('./main.js', () => {
+        console.log('Accept update！')
+    })
+}
