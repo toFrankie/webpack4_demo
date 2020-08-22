fcac271d5a540a0e87d37808d01b1db379381397
Frankie <1426203851@qq.com>
2020-08-23 03:24:17 +0800

    add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,11 @@ const config = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
         test: /\.css/,
         use: ['style-loader', 'css-loader']
       }
