017a2fc4fd18314d748e570fb51bd43e561d0694
Frankie <1426203851@qq.com>
2020-08-23 12:07:29 +0800

    调整项目结构

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
-import './main.js'
-import './style.css'
+import '../styles/style.css'
 
 // 最简单的 React 示例
 const rootElem = document.getElementById('app')
 render(<div>Hello React!</div>, rootElem)
-
-if(module.hot) {
-    module.hot.accept('./main.js', () => {
-        console.log('Accept update！')
-    })
-}
--- src/main.js
+++
@@ -1,2 +0,0 @@
-console.log('This is main.js.')
-console.log('We modified the main.js file.')
