ac07c86c2e711168924b467b84f417c1df0f22dd
Frankie <1426203851@qq.com>
2020-09-06 05:54:05 +0800

    使用 combineReducers 对 reducer 进行拆分

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,17 +1,10 @@
-// Reducer 处理函数
-const reducer = (prevState, action) => {
-  const { type, payload } = action
-  switch (type) {
-    case 'ADD':
-      // 一定要不能修改 state，而是返回一个新的副本
-      // 倘若 state 是引用数据类型，一定要借助 Object.assign、对象展开运算符(...)、其他库的拷贝方法或者自己实现深拷贝方法，返回一个新副本
-      return { ...prevState, count: prevState.count + payload }
-    case 'SUB':
-      return { ...prevState, count: prevState.count - payload }
-    default:
-      // default 或者未知 action 时，返回旧的 state
-      return prevState
-  }
-}
+import { combineReducers } from 'redux'
+import countReducer from './countReducer'
+import statusReducer from './statusReducer'
 
-export default reducer
+const rootReducer = combineReducers({
+  count: countReducer,
+  status: statusReducer
+})
+
+export default rootReducer
