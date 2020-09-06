ba98b56ef2bbbefdcad9f14bb0723f2c63ff9398
Frankie <1426203851@qq.com>
2020-09-06 22:44:06 +0800

    将配置 redux-saga 修改为 redux-thunk

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,26 +1,27 @@
 import { createStore, applyMiddleware, compose } from 'redux'
-import createSagaMiddleware from 'redux-saga'
+// import createSagaMiddleware from 'redux-saga'
+import thunkMiddleware from 'redux-thunk'
 import logger from 'redux-logger'
-import rootSaga from '../sagas'
+// import rootSaga from '../sagas'
 import reducers from '../reducers'
 
 // 初始值
 const initialState = { count: 0, status: 'offline' }
 
 // 创建 saga middleware
-const sagaMiddleware = createSagaMiddleware()
+// const sagaMiddleware = createSagaMiddleware()
 
 // 当使用 middleware 时，我们需要使用 window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ 作判断了
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
 
 // 判断是否含有 Redux DevTools 插件
-const middlewares = typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ ? applyMiddleware(sagaMiddleware) : applyMiddleware(sagaMiddleware, logger)
+const middlewares = typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ ? applyMiddleware(thunkMiddleware, logger) : applyMiddleware(thunkMiddleware, logger)
 
 // 创建 Store（也可以不传入 initialState 参数，而将 reducer 中的 state 设置一个初始值）
 const store = createStore(reducers, initialState, composeEnhancers(middlewares))
 
 // 启动 saga
-sagaMiddleware.run(rootSaga)
+// sagaMiddleware.run(rootSaga)
 
 // 监听 state 变化
 // const unsubscribe = store.subscribe(() => {
