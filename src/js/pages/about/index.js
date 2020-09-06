5559a866a43cd5d3c7e05f2410de204633779a3d
Frankie <1426203851@qq.com>
2020-09-06 22:43:31 +0800

    调整 About 组件

--- src/js/pages/about/index.js
+++ src/js/pages/about/index.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { getUser } from '../../actions/userActions'
 
 class About extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {}
-    }
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
 
-    render() {
-        return <div>About Component！</div>
-    }
+  render() {
+    return (
+      <div>
+        <h3>About Component！</h3>
+        <h5>Get User: {this.props.user.status || ''}</h5>
+        {/* 我们发现这里并不是传了一个标准的 Action 对象，而是一个函数 */}
+        <button onClick={() => { this.props.dispatch(getUser(false)) }}>Get User Fail</button>
+        <button onClick={() => { this.props.dispatch(getUser(true)) }}>Get User Success</button>
+      </div>
+    )
+  }
 }
 
-export default About
+const mapStateToProps = state => {
+  return { user: state.user }
+}
+
+export default connect(mapStateToProps)(About)
