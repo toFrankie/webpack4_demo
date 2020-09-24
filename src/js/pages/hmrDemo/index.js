c12773bdf6d1f816c2515b2189ec15e45562a995
Frankie <1426203851@qq.com>
2020-09-24 16:20:46 +0800

    feat: HMR 测试页面

---
+++ src/js/pages/hmrDemo/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+class HMRDemo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>HMR Demo Component!</h3>
+        <h5>Add some node element</h5>
+        <h5>Add some node element</h5>
+        <h5>Add some node element</h5>
+        <h5>计数器：{this.state.count}</h5>
+        <button onClick={() => { this.setState({ count: ++this.state.count }) }}>add</button>
+      </div>
+    )
+  }
+}
+
+export default HMRDemo
--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from './home'
 export { default as About } from './about'
+export { default as HMRDemo } from './hmrDemo'
 export { default as NotFound } from './404'
