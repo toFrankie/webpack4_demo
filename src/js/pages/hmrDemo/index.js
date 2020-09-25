f44e0b2d31d672707fa40887451a07b22b187177
Frankie <1426203851@qq.com>
2020-09-26 00:48:10 +0800

    style: 更新

--- src/js/pages/hmrDemo/index.js
+++ src/js/pages/hmrDemo/index.js
@@ -13,8 +13,6 @@ class HMRDemo extends Component {
       <div>
         <h3>HMR Demo Component!</h3>
         <h5>Add some node element</h5>
-        <h5>Add some node element</h5>
-        <h5>Add some node element</h5>
         <h5>计数器：{this.state.count}</h5>
         <button onClick={() => { this.setState({ count: ++this.state.count }) }}>add</button>
       </div>
