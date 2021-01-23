caab0f1a6eff36065e7173ec6b011e55e9128b25
Frankie <1426203851@qq.com>
2021-01-23 15:10:10 +0800

    test: 删除无用文件

--- commons/js/components/selectCar/index.js
+++
@@ -1,3 +0,0 @@
-export { default as ListCars } from './ListCars' // 车型列表 - 所有字母
-export { default as Cars } from './Cars' // 车型列表 单个字母
-export { default as ListKey } from './ListKey' // 字母索引
--- commons/js/lib/exif-rotate-js/options.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.defaultOptions = {
-    maxSize: 720,
-    quality: 1
-};
