5f092c410954d0a8a0197dcaf0bf5dc4d015fe36
Frankie <1426203851@qq.com>
2021-06-03 15:52:30 +0800

    feat: 增加 XHR 示例

--- src/js/pages/about/index.js
+++ src/js/pages/about/index.js
@@ -8,6 +8,30 @@ class About extends Component {
     this.state = {}
   }
 
+  xhrRequest() {
+    // 创建 xhr 对象
+    const xhr = new XMLHttpRequest()
+
+    // 通过 onreadystatechange 事件捕获请求状态的变化
+    // 必须在 open 之前指定该事件，否则无法接收 readyState 0 和 1 的状态
+    xhr.onreadystatechange = () => {
+      console.log(xhr.status)
+      console.log(xhr.readyState)
+
+      console.log(xhr.getResponseHeader('FooBar'))
+    }
+
+    xhr.onerror = err => {
+      console.log('error:', err)
+    }
+
+    // 初始化请求
+    xhr.open('GET', 'http://10.16.4.226:7701/config', true)
+
+    // 发送请求
+    xhr.send(null)
+  }
+
   render() {
     return (
       <div>
@@ -19,6 +43,13 @@ class About extends Component {
           }}
         >
           Fetch Data
+        </button>
+        <button
+          onClick={() => {
+            this.xhrRequest()
+          }}
+        >
+          XHR Request
         </button>
       </div>
     )
